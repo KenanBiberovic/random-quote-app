20a345f82a3c9d292424bbfb5e88f180cc93f561
Kenan Biberovic <kenanbiberovic020@gmail.com>
2023-05-15 23:54:54 +0200

    random quote app

---
+++ src/Components/Card.js
@@ -0,0 +1,13 @@
+import react, { Component } from "react";
+
+class card extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="quote-wrap">
+          <button className="get-quo"></button>
+        </div>
+      </div>
+    );
+  }
+}
